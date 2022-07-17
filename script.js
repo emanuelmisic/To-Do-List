440cbc3ed9778a0b76a85d2b5670046c1765edb5
emanuelmisic <emanuelmisic64@gmail.com>
2022-07-17 22:46:30 +0200

    Add partial list display functionality

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const makeListBtn = document.querySelector('#makeListBtn');
+const container = document.querySelector('.container');
 
 const promptDiv = document.querySelector('.prompt');
 
 const listNameInput = document.querySelector('#listName');
-const dueDateOutput = document.querySelector('#dueDate');
+const dueDateInput = document.querySelector('#dueDate');
 const expandListBtn = document.querySelector('#addListInput');
 const generateListBtn = document.querySelector('#generateListBtn');
 
 makeListBtn.onclick = () => listPrompt();
-generateListBtn.onclick = () => generateList();
 
 function listPrompt() {
+  const list = [];
   promptDiv.classList.remove('hide');
+  listNameInput.value = '';
+  dueDateInput.value = '';
+
+  generateListBtn.onclick = () =>
+    generateList(listNameInput.value, list, dueDateInput.value);
 }
 
-function generateList() {}
+function generateList(name, list, dueDate) {
+  let listName = name;
+  let itemList = list;
+  let date = dueDate;
+  promptDiv.classList.add('hide');
+  displayList(listName, itemList, date);
+}
+
+function displayList(name, list, dueDate) {
+  const listContainer = document.createElement('div');
+  listContainer.classList.add('list');
+  const listName = document.createElement('h3');
+  listName.classList.add('listName');
+
+  listContainer.appendChild(listName);
+  container.appendChild(listContainer);
+
+  listName.innerHTML = name;
+}
